2d5ebb4d877a90129a50c59fe03cfa1b4d0de78e
Lakshay728 <lakshaymudgil18102005@gmail.com>
2025-11-23 22:30:33 +0530

    initial commit

---
+++ helo.py
@@ -0,0 +1,18 @@
+#Task 1
+"""def facto(a):
+    if a == 0 or a == 1:
+        return 1
+    else:
+        return a * facto(a - 1)
+
+num = int(input("Give a number: "))
+print(f"Factorial of {num} is {facto(num)}")"""
+
+#Task 2
+import math
+
+num = float(input("Enter a number: "))
+
+print("Square root =", math.sqrt(num))
+print("Natural log (ln) =", math.log(num))
+print("Sine (in radians) =", math.sin(num))
